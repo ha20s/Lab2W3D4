6402369064ae153e41680180f52bed0c9389fa4e
Hasna <hassna.ahmad98@gmail.com>
2024-09-11 14:57:21 +0300

    modify

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
 
 
 let colors = ["lightblue" , "purple" , "lightgreen" , "pink"]
-let randomColor = Math.floor(Math.random() * colors.length)
+// let randomColor = Math.floor(Math.random() * colors.length)
 // console.log(colors[randomColor] ) 
 
 
 let heights = ["10vh" , "20vh" , "30vh" , "40vh"]
 
-let randomHeight = Math.floor(Math.random() * heights.length)
+// let randomHeight = Math.floor(Math.random() * heights.length)
 // console.log(heights[randomHeight])
 
 
 let widths = ["10vw" , "20vw" , "30vw" , "40vw"]
-let randomWidth = Math.floor(Math.random() * widths.length)
+// let randomWidth = Math.floor(Math.random() * widths.length)
 // console.log(widths[randomWidth])
 
 let positions = ["top" , "center", "left" , "bottom" , "right"]
 // let positions = ["10" , "20", "30" , "40" , "50"]
 
-let randomPosition = Math.floor(Math.random() * positions.length)
+// let randomPosition = Math.floor(Math.random() * positions.length)
 // console.log(positions[randomPosition])
 
 let cube = document.getElementById("cube")
 
 function randomStyle() {
-    cube.style.backgroundColor= colors[randomColor]
-    cube.style.width = widths[randomWidth]
-    cube.style.height = heights[randomHeight]
-    cube.style.float = positions[randomPosition] // check it 
+    cube.style.backgroundColor= colors[Math.floor(Math.random() * colors.length)]
+    cube.style.height = heights[Math.floor(Math.random() * heights.length)]
+    cube.style.width =  widths[Math.floor(Math.random() * widths.length)]
+    cube.style.display = "flex"
+    cube.style.alignItems = positions[Math.floor(Math.random() * positions.length)] // check it 
     }
 
     setInterval(randomStyle, 1000);
